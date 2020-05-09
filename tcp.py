64813f06135709a0a24f3a47d5c093c74af9a39e
Z3338 <61259788+Z3338@users.noreply.github.com>
2020-05-09 10:39:21 +0800

    Update tcp.py

--- tcp.py
+++ tcp.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 import socket
 import struct
 import pickle
@@ -6,7 +7,9 @@ import os
 import zipfile
 import sys
 
-# file_dir = os.path.dirname(os.path.abspath(__file__))
+# server_ip = '192.168.1.2'
+# server_port = 5200
+file_path = os.path.dirname(os.path.abspath(__file__))
 header_struct = struct.Struct('i1024s')
 data_struct = struct.Struct('1024s')
 
@@ -16,7 +19,7 @@ def print_progress(percent, width=50):
         percent = 100
     # 字符串拼接的嵌套使用
     show_str = ('[%%-%ds]' % width) % (int(width * percent / 100) * '>')
-    print('\r%s %d%%' % (show_str, percent), end='')
+    print('\r%s %d%%' % (show_str, percent))
 
 
 def get(file_dir, client):
@@ -24,19 +27,23 @@ def get(file_dir, client):
     packed_header = client.recv(1024 + 4)
     # 解包得到序列化的header的长度和header正文
     header_size, header_s = header_struct.unpack(packed_header)
+    # 大小为0则说明查无此文件
     # 反序列化得到header正文
     header = pickle.loads(header_s)
     file_name = header['file_name']
     file_size = header['file_size']
     folder_path, name = os.path.split(file_name)
-    with open('%s\\%s' % (file_dir, name), 'wb') as f:
+    filename = os.path.join(file_dir, name)
+    print(filename)
+    with open(filename, 'wb') as f:
+        # print('%s\\%s' % (file_dir, name))
         recv_size = 0
         while recv_size < file_size:
             res = client.recv(1024)
             f.write(res)
             recv_size += len(res)
             recv_per = int(recv_size / file_size * 100)
-            print_progress(recv_per)
+            # print_progress(recv_per)
     return name
 
 
@@ -57,17 +64,13 @@ def run(path, server_ip, server_port):
     while True:
         client, client_addr = server.accept()
         print('A new connection from %s' % client_addr[0])
-        while True:
-            try:
-                # print('Send %s to %s' % (request, client_addr[0]))
-                name = get(path, client)
-                unzip_file(path+'\\'+name, path)
-                os.remove(path+'\\'+name)
-            except ConnectionResetError:
-                break
+        # print('Send %s to %s' % (request, client_addr[0]))
+        name = get(file_path, client)
+        unzip_file(os.path.join(file_path, name), path)
+        os.remove(os.path.join(file_path, name))
         client.close()
     server.close()
 
 
 if __name__ == '__main__':
-    run(sys.argv[1],sys.argv[2],int(sys.argv[3]))
+    run(sys.argv[1], sys.argv[2], int(sys.argv[3]))
